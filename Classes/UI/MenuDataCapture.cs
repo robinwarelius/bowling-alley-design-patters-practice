b962718bd9822684d1caef8a6d0e6790f1ca4261
Robin Stahl Warelius <107325879+robinwarelius@users.noreply.github.com>
2023-02-22 09:08:56 +0100

    Added simulating game to option menu

--- Classes/UI/MenuDataCapture.cs
+++ Classes/UI/MenuDataCapture.cs
@@ -19,6 +19,7 @@ namespace BengansBowlingApplikation.UIclasses
                 Console.WriteLine("3. Registrera Tävlingsdeltagare");
                 Console.WriteLine("4. Registrera Tävlingsresultat");
                 Console.WriteLine("5. Poängställning");
+                Console.WriteLine("6. Simulera Match");
                 choice = Convert.ToInt32(Console.ReadLine());
                 return choice;
             } catch (Exception ex)
